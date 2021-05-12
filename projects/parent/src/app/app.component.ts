dace25e85da1a241755c7db5ac0c8c69edb72452
ywarezk <ywarezk@gmail.com>
2021-05-12 19:15:51 +0300

    created the parent app

---
+++ projects/parent/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>
+      Hello parent app
+    </h1>
+  `,
+})
+export class AppComponent {
+}
